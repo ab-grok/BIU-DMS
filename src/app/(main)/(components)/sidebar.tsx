b665c4d3a2c4d9ae4f8e319d54168bdbef09405b
AB <absule032@gmail.com>
2025-05-19 04:23:24 +0100

    debugging

--- src/app/(main)/(components)/sidebar.tsx
+++ src/app/(main)/(components)/sidebar.tsx
@@ -38,28 +38,26 @@ export default function SideBar() {
       className={` ${(sidebarState.sbExpanded ?? true) && sidebarState.route ? "relative left-[5%] max-w-[15rem] min-w-[15rem]" : sidebarState.route ? "relative left-[5%] w-[3rem] max-w-[3rem] lg:max-w-[15rem] lg:min-w-[15rem]" : "absolute left-[8%] w-full max-w-[15rem] sm:left-[25%] md:left-[25%] md:max-w-[30rem] lg:left-[33.3%]"} bg-main-bg z-5 flex h-[40rem] max-h-[100%] items-center justify-center rounded-[5px] shadow-lg shadow-black transition-all`}
     >
       {(isLoading.includes("sidebar") || !sidebarEditable) && <Loading />}
-      <main className="bg-main-fg ring-main-bg/50 relative flex h-[99.8%] w-[99.2%] flex-col overflow-hidden rounded-[5px] ring-2">
+      <main className="bg-main-fg/30 ring-main-bg/50 relative flex h-[99.8%] w-[99.2%] flex-col overflow-hidden rounded-[5px] ring-2">
         <section
-          className={`group relative top-0 h-full max-h-[15%] min-h-[15%] w-full bg-blue-500/20`}
+          className={`group bg-main-fg relative top-0 h-full max-h-[15%] min-h-[15%] w-full items-center justify-center rounded-[20px]`}
         >
-          <div className="relative flex h-full w-[100%] items-center justify-center bg-amber-300">
-            <div
-              onBlur={() => showAvatar(1)}
-              id="avatar"
-              className={`ring-main-bg flex ring-2 ${quickActions ? "h-[5rem] w-[6rem] rounded-[5px]" : !sidebarState.sbExpanded ? "size-[3rem] rounded-full lg:size-[5rem]" : "size-[5rem] rounded-full"} ${!sidebarState.sbExpanded && ""} bg-main-fg shadow-shadow-bw text-bw cursor-pointer items-center self-center overflow-hidden text-xs shadow-md transition-all duration-200`}
-              onClick={() => showAvatar()}
-            >
-              {!quickActions ? (
-                <Avatar expanded={sidebarState.sbExpanded} />
-              ) : (
-                <SBQuickActions fn2={() => logOut()} />
-              )}
-            </div>
+          <div
+            onBlur={() => showAvatar(1)}
+            id="avatar"
+            className={`ring-main-bg flex ring-2 ${quickActions ? "h-[5rem] w-[6rem] rounded-[5px]" : !sidebarState.sbExpanded ? "size-[3rem] rounded-full lg:size-[5rem]" : "size-[5rem] rounded-full"} ${!sidebarState.sbExpanded && ""} bg-main-fg shadow-shadow-bw text-bw cursor-pointer items-center self-center overflow-hidden text-xs shadow-md transition-all duration-200`}
+            onClick={() => showAvatar()}
+          >
+            {!quickActions ? (
+              <Avatar expanded={sidebarState.sbExpanded} />
+            ) : (
+              <SBQuickActions fn2={() => logOut()} />
+            )}
           </div>
         </section>
         <Separator className="bg-card-background max-w-[95%] self-center" />
 
-        <section className="bg-main-fg relative top-0 h-full max-h-[60%] w-full flex-none">
+        <section className="bg-main-fg relative top-0 h-full max-h-[60%] w-full flex-none rounded-[20px]">
           {" "}
           <SideCard name="Databases" route="databases" />
           <SideCard name="Create a database" route="createdb" />
@@ -68,7 +66,7 @@ export default function SideBar() {
           <SideCard name="Users" route="users" />
         </section>
         <Separator className="bg-card-background max-w-[95%] self-center" />
-        <section className="relative top-0 h-full max-h-[25%] w-full bg-amber-500/20">
+        <section className="bg-main-fg relative top-0 h-full max-h-[25%] w-full rounded-[20px]">
           {" "}
           List of he db tag (get current db route)
         </section>
@@ -84,9 +82,9 @@ function Avatar({ expanded }: { expanded: boolean }) {
       title="Click to show actions"
       id="avatar"
       onClick={() => setSidebarState((p) => ({ ...p, sbExpanded: true }))}
-      className={`${!expanded ? "size-[3rem] lg:size-[5rem]" : "size-[5rem]"} bg-main-bg text-bw absolute rounded-full p-1 text-xs transition-all`}
+      className={`${!expanded ? "size-[3rem] lg:size-[5rem]" : "size-[5rem]"} bg-main-bg text-bw absolute flex items-center justify-center rounded-full p-1 text-xs transition-all`}
     >
-      <FaUserAlt />
+      <FaUserAlt className="fill-bg-sub-grad size-4 stroke-1" />
     </div>
   );
 }
