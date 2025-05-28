bf9450c8f4d85a1be1f17e9cec53a13c8a50cc0b
AB <absule032@gmail.com>
2025-05-28 05:33:25 +0100

    debugging

--- src/app/(main)/(pages)/databases/[database]/(components)/createtb.tsx
+++ src/app/(main)/(pages)/databases/[database]/(components)/createtb.tsx
@@ -335,7 +335,7 @@ export default function CreateTb({ i, uData, db }: tbType) {
                     ref={colScroll}
                     className="scrollbar-custom flex h-full max-w-[20rem] min-w-[20rem] gap-3 overflow-x-scroll transition-all duration-500"
                   >
-                    {createTbCol.filter(Boolean).length &&
+                    {createTbCol.length > 0 &&
                       createTbCol.map((a, i) => (
                         <UserTag
                           key={a.name}
