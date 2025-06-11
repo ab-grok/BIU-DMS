3f8529e6c4d2f506a0790bed3c794f73ef5d9b35
AB <absule032@gmail.com>
2025-06-11 15:06:53 +0100

    Debugging

--- src/app/(main)/(pages)/databases/[database]/[table]/page.tsx
+++ src/app/(main)/(pages)/databases/[database]/[table]/page.tsx
@@ -28,58 +28,58 @@ export default function TableRows() {
   React.useEffect(() => {
     (async () => {
       console.log("in getTb rowHeader currTb,: ", tbPath);
-      // const { tbSchema, error1 } = await getTableSchema(dbName, tbName);
-      // console.log("in [table] got tbSchema: ", tbSchema);
-      // const { tbData, error } = await getTableData(dbName, tbName);
-      // console.log("in [table] got tbData: ", tbData);
-      // if (error || error1) {
-      //   setNotify({ message: error1 || error, danger: true });
-      //   return;
-      // }
-      // let currTb: rcData | undefined = rc.find((a) => (a.tbPath = tbPath));
-      // const placeTb = {
-      //   tbPath: tbPath,
-      //   rcRows: [] as rowData[],
-      //   rcHeader: [] as colSchema[],
-      // };
-      // setRc((p) => {
-      //   let updRc = [] as rcData[];
-      //   let rcFound = false;
-      //   for (const [i, a] of p.entries()) {
-      //     if (a.tbPath == tbPath) {
-      //       rcFound = true;
-      //       if (a.rcHeader.length != tbSchema?.length) {
-      //         currTb = {
-      //           ...(currTb || placeTb),
-      //           rcHeader: tbSchema as colSchema[],
-      //         };
-      //         updRc = [...p.slice(0, i), currTb, ...p.slice(i + 1)].filter(
-      //           Boolean,
-      //         );
-      //       }
-      //       if (a.rcRows.length != tbData?.length) {
-      //         currTb = {
-      //           ...(currTb || placeTb),
-      //           rcRows: tbData as rowData[],
-      //         };
-      //         updRc = [...p.slice(0, i), currTb, ...p.slice(i + 1)].filter(
-      //           Boolean,
-      //         );
-      //       }
-      //     }
-      //   }
-      //   if (!rcFound) {
-      //     currTb = {
-      //       ...(currTb || placeTb),
-      //       rcRows: tbData as rowData[],
-      //       rcHeader: tbSchema as colSchema[],
-      //     };
-      //     updRc = [...p, { ...(currTb as rcData) }].filter(Boolean);
-      //   }
-      //   if (updRc.length) return updRc;
-      //   else return p;
-      // });
-      // console.log("in [table] got past setRc, currTb: ", currTb);
+      const { tbSchema, error1 } = await getTableSchema(dbName, tbName);
+      console.log("in [table] got tbSchema: ", tbSchema);
+      const { tbData, error } = await getTableData(dbName, tbName);
+      console.log("in [table] got tbData: ", tbData);
+      if (error || error1) {
+        setNotify({ message: error1 || error, danger: true });
+        return;
+      }
+      let currTb: rcData | undefined = rc.find((a) => (a.tbPath = tbPath));
+      const placeTb = {
+        tbPath: tbPath,
+        rcRows: [] as rowData[],
+        rcHeader: [] as colSchema[],
+      };
+      setRc((p) => {
+        let updRc = [] as rcData[];
+        let rcFound = false;
+        for (const [i, a] of p.entries()) {
+          if (a.tbPath == tbPath) {
+            rcFound = true;
+            if (a.rcHeader.length != tbSchema?.length) {
+              currTb = {
+                ...(currTb || placeTb),
+                rcHeader: tbSchema as colSchema[],
+              };
+              updRc = [...p.slice(0, i), currTb, ...p.slice(i + 1)].filter(
+                Boolean,
+              );
+            }
+            if (a.rcRows.length != tbData?.length) {
+              currTb = {
+                ...(currTb || placeTb),
+                rcRows: tbData as rowData[],
+              };
+              updRc = [...p.slice(0, i), currTb, ...p.slice(i + 1)].filter(
+                Boolean,
+              );
+            }
+          }
+        }
+        if (!rcFound) {
+          currTb = {
+            ...(currTb || placeTb),
+            rcRows: tbData as rowData[],
+            rcHeader: tbSchema as colSchema[],
+          };
+          updRc = [...p, { ...(currTb as rcData) }].filter(Boolean);
+        }
+        if (updRc.length) return updRc;
+        else return p;
+      });
+      console.log("in [table] got past setRc, currTb: ", currTb);
     })();
   }, [created.rc, created.rh]);
 
