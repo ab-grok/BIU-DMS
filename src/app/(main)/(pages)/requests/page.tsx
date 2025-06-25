fc74c7e75922c4a755f02d28099591761b472325
AB <absule032@gmail.com>
2025-06-25 09:05:07 +0100

    Fixed bugs

--- src/app/(main)/(pages)/requests/page.tsx
+++ src/app/(main)/(pages)/requests/page.tsx
@@ -1,6 +1,8 @@
+import { getRequests } from "@/lib/server";
+
 export default function Requests() {
   function getVERequests() {
-    const { dbRequests, tbRequests } = getRequests();
+    // const { dbRequests, tbRequests } = getRequests();
   }
   return <div>view requests</div>;
 }
