78ad453708edceef184538dd8c2cf597d87cf74d
AB <absule032@gmail.com>
2025-06-25 09:38:36 +0100

    Fixed bugs

--- src/lib/server.js
+++ src/lib/server.js
@@ -788,7 +788,7 @@ export async function insertTbData({ dbName, tbName, colVals, token32 }) {
       values.push(main`${val}`);
     }
     // console.log("````````` After vals loop , values: ", values, "`````````");  //got here
-    valuesArr.push(main`(${values}, ${now}, ${udata} )`);
+    valuesArr.push(main`(${main.array(values)}, ${now}, ${udata} )`);
   }
   console.log("in insertTbData, udata: ", udata);
   // console.log(
