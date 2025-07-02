e1bf6851743c9a1cd5b872589372325607019915
AB <absule032@gmail.com>
2025-07-02 11:50:47 +0100

    Fixed bugs

--- src/app/(main)/(pages)/databases/[database]/[table]/(components)/rows.tsx
+++ src/app/(main)/(pages)/databases/[database]/[table]/(components)/rows.tsx
@@ -296,12 +296,13 @@ export function RowItem({
   }
 
   function isPgFile(val: any): val is pgFile {
-    console.log("isPgFile, val: ", val);
+    // console.log("isPgFile, val: ", val);
 
     const v = val.split(",");
 
     console.log("v[0]/fileName from pg: ", v[0]);
     console.log("v[1]/fileData from pg: ", v[1]);
+    console.log("indexof v[1]/fileData from pg: ", v[1].indexOf("x"));
     console.log("v[2]/fileType from pg: ", v[2]);
     // console.log("v from pg: ", v);
     return v && Array.isArray(v) && v[2] == "string" && v[2].includes("/");
