0fa98ae31772d03aa4932073bff894a5c020ebee
AB <absule032@gmail.com>
2025-07-02 04:29:42 +0100

    Fixed bugs

--- src/app/(main)/(pages)/databases/[database]/[table]/(components)/rows.tsx
+++ src/app/(main)/(pages)/databases/[database]/[table]/(components)/rows.tsx
@@ -400,7 +400,7 @@ export function RowItem({
               onDrop={fileDropped}
               tabIndex={0}
               onBlur={field?.onBlur || clickedOut}
-              className={`group/fd flex p-1 ${fileDrag ? "animate-logoExit bg-red-600/30 shadow-xs" : "bg-bw/10 shadow-sm"} ${pressAnim.includes("uplc") && "scale-95"} rounded-xl transition-all`}
+              className={`group/fd flex p-1 ${fileDrag ? "animate-logoExit bg-red-600/30 shadow-xs" : "shadow-sm"} ${pressAnim.includes("uplc") && "scale-95"} rounded-xl transition-all`}
             >
               {canEdit && editMode ? (
                 <Label className="group-hover/fd:text-bw text-bw/80 text-[10px]">
